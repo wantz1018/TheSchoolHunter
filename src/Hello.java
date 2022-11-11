67792e29893789b4446337b6aa154e9e17c2511f
wantz <wantz@foxmail.com>
2022-11-11 22:36:00 +0800

    添加注解

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
+@WebServlet(name = "Hello", value = "/Hello")
 public class Hello extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
