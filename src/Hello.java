10fb6eacef0505ecb89734b7741f0f64f87f8e56
wantz <wantz@foxmail.com>
2022-11-11 22:13:57 +0800

    添加Hello的Servlet

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Hello", value = "/Hello")
+public class Hello extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("Hello");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
