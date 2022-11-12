46baeea3477b88f9cb8ec9e1698ad4cc225f295f
wantz <wantz@foxmail.com>
2022-11-12 15:55:54 +0800

    删除测试接口。添加正式接口

---
+++ src/servlet/MissionList.java
@@ -0,0 +1,34 @@
+package servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "missionlist", value = "/missionlist")
+public class MissionList extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json;charset=utf-8");
+        response.setCharacterEncoding("UTF-8");
+        String jsonStr = "{\"missionlist\":[{" +
+                "\"id\":\"1\"," +
+                "\"icon\":\"https://lptexas.top/img/cover/default_cover.jpg\"," +
+                "\"title\":\"我是标题\"," +
+                "\"content\":\"我是一段任务描述\"," +
+                "\"mdate\":\"2022-11-10\"," +
+                "\"mplace\":\"三江楼1217\"," +
+                "\"rewards\":\"10\"" +
+                "}]}";
+        PrintWriter res;
+        res = response.getWriter();
+        res.write(jsonStr);
+        res.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
