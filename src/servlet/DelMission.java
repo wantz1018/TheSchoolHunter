990b1109dbdd924aca0bd7fb8366fb34c497c907
wantz <wantz@foxmail.com>
2022-12-26 16:43:03 +0800

    访问添加操作者ID

--- src/servlet/DelMission.java
+++ src/servlet/DelMission.java
@@ -13,7 +13,8 @@ import java.sql.SQLException;
 public class DelMission extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String id = request.getParameter("id");
+        String uid = request.getParameter("u_id");
+        String id = request.getParameter("m_id");
         String sql = "update ttasks set status = '已结束' where m_id = ? and check_status = '已通过'";
         String str = "";
         try {
