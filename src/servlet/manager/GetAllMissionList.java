044b8ac924da5c354e6fdad89243abe8c4ccf7c5
wantz <wantz@foxmail.com>
2022-12-28 16:55:10 +0800

    添加管理员专用API

--- src/servlet/manager/GetAllMissionList.java
+++ src/servlet/manager/GetAllMissionList.java
@@ -17,7 +17,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-@WebServlet(name = "getAllMissionList", value = "/getAllMissionList")
+@WebServlet(name = "getAllMissionList", value = "/api/getAllMissionList")
 public class GetAllMissionList extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
